1b749ea7d6a9ef77bc186ec5639675667b29062d
DEbu-17 <98536414+DEbu-17@users.noreply.github.com>
2023-09-18 12:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+   let binary = '';
+  while (decimalNumber > 0) {
+    const remainder = decimalNumber % 2;
+    binary = remainder + binary; // Add the remainder to the beginning
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
